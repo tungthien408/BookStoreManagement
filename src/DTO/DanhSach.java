a3dd775ff27390c82e566c288d0999ddabbdd43e
tungthien408 <142231020+tungthien408@users.noreply.github.com>
2025-03-28 12:53:08 +0000

    Delete src/DTO directory

--- src/DTO/DanhSach.java
+++
@@ -1,7 +0,0 @@
-package DTO;
-
-public interface DanhSach {
-    public String getID();
-    public void xuatTieuDe();
-    public void xuatDS();
-} 
--- src/DTO/DanhSachDTO.java
+++
@@ -1,7 +0,0 @@
-package DTO;
-
-public interface DanhSachDTO {
-    public String getID();
-    public void xuatTieuDe();
-    public void xuatDS();
-} 
